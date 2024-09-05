e2690ee7e5569adda9506cdf3138572e740444be
Boris Boinier <81011303+SaukratesK@users.noreply.github.com>
2024-09-05 16:27:51 +0200

    modify one letter in ascii script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,4 +1,4 @@
 sudo apt install cowsay -y
-cowsay -f dragon "Run for cover, I am a DRAGON...RAWR" >> dragon.txt
+cowsay -f dragon "Run for cover, I am a DRAGON...RAAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
